4475e105413c6817ea82cb0a46d298135c623167
conqsolid <conqsolid@gmail.com>
2019-12-14 23:07:04 +0300

    play different sounds

--- lib/main.dart
+++ lib/main.dart
@@ -4,20 +4,81 @@ import 'package:flutter/material.dart';
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+  void playNote(int noteNumber) {
+    final player = new AudioCache();
+    player.play('note$noteNumber.wav');
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Center(
-            child: FlatButton(
-              onPressed: () {
-                final player = new AudioCache();
-                player.play('note1.wav');
-              },
-              child: Text("Click me"),
+          child: Column(children: [
+            Expanded(
+              child: FlatButton(
+                color: Colors.red,
+                onPressed: () {
+                  playNote(1);
+                },
+                child: Text("Click me"),
+              ),
             ),
-          ),
+            Expanded(
+              child: FlatButton(
+                color: Colors.green,
+                onPressed: () {
+                  playNote(2);
+                },
+                child: Text("Click me"),
+              ),
+            ),
+            Expanded(
+              child: FlatButton(
+                color: Colors.blue,
+                onPressed: () {
+                  playNote(3);
+                },
+                child: Text("Click me"),
+              ),
+            ),
+            Expanded(
+              child: FlatButton(
+                color: Colors.orange,
+                onPressed: () {
+                  playNote(4);
+                },
+                child: Text("Click me"),
+              ),
+            ),
+            Expanded(
+              child: FlatButton(
+                color: Colors.yellow,
+                onPressed: () {
+                  playNote(5);
+                },
+                child: Text("Click me"),
+              ),
+            ),
+            Expanded(
+              child: FlatButton(
+                color: Colors.grey,
+                onPressed: () {
+                  playNote(6);
+                },
+                child: Text("Click me"),
+              ),
+            ),
+            Expanded(
+              child: FlatButton(
+                color: Colors.indigo,
+                onPressed: () {
+                  playNote(1);
+                },
+                child: Text("Click me"),
+              ),
+            ),
+          ]),
         ),
       ),
     );
